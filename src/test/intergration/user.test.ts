ebcc4bab322113174689c7a5c5fd5275cd67ba3c
SANGHWA LEE <show10033@naver.com>
2022-06-09 15:09:18 +0900

    test-intergration of createUser edit

--- src/test/intergration/user.test.ts
+++ src/test/intergration/user.test.ts
@@ -2,32 +2,31 @@ import request from "supertest";
 import app from "../../.."
 import assert from 'assert';
 const testUser = {
-	id:"asd@sddssssssssd.com",
-	password:'12345678aA'
+	id:"test@test.com",
+	password:'Ab456456456'
 }
 
-// describe('POST /auth/user', function() {
-//   it('createUser', function(done) {
-//     request(app)
-//     .post('/auth/users')
-//     .send(testUser)
-//     .expect('Content-Type',/json/)
-//     .expect(200,done)
-//     .end();
-//   });
-// }); 
-
-describe('POST /auth/user/status', function() {
-  it('login', function(done) {
+describe('POST /auth/user', function() {
+  it('createUser', function(done) {
     request(app)
-    .post('/auth/users/status')
+    .post('/auth/users')
     .send(testUser)
     .expect('Content-Type',/json/)
-    .expect(200)
-    .then(response => {
-      assert(response.body.data, testUser.id);
-      done();
-    })
-  })
-});
+    .expect(200,done)
+  });
+}); 
+
+// describe('POST /auth/user/status', function() {
+//   it('login', function(done) {
+//     request(app)
+//     .post('/auth/users/status')
+//     .send(testUser)
+//     .expect('Content-Type',/json/)
+//     .expect(200)
+//     .then(response => {
+//       assert(response.body.data, testUser.id);
+//       done();
+//     })
+//   })
+// });
 
